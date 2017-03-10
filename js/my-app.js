828aeeb59f5a78f7f36962e8708541d2d238822b
kashifsultani84 <kashifsultani84@gmail.com>
2017-03-10 11:21:23 +0500

    mar14

--- js/my-app.js
+++ js/my-app.js
@@ -218,7 +218,7 @@ $$(document).on('pageInit', function (e) {
 						 if(key == 'coupon_id')
 							data 	+= 	'<div class="row main-store-content no-gutter">'
 									+		'<div class="inner-row-main-store-content">'
-									+				'<div class="col-50 stores-heading">'
+									+				'<div class="col-8 stores-heading">'
 									+					'<h2 class="no-margin"><a href="coupon-detail.html?coupon_id=' + result[i].coupon_id +'">' + result[i].coupon_title +'</a></h2>'
 									+				'</div>'
 									+				'<div class="col-20 save">'
@@ -270,15 +270,61 @@ $$(document).on('pageInit', function (e) {
 			success: function (result) {
 					
 					var data = '';
+					var store_heading ='';
+					var store_logo_main= '';
+					var store_heading_main = '';
+					
+					var end_date = '';
+					var success_ration = '';
+					
 					for (var i=0 ; i <= (result.length-1); i++) {
 					   
 					   $.each( result[i], function( key, value ) {
+						  //data  += 	key + '  =  ' + value;
+						  if(result[i].coupon_expiry_date != null)
+							end_date = 'Expiry: '+ result[i].coupon_expiry_date;
+						 
+						  if(result[i].success_ration != undefined)
+							success_ration = result[i].success_ration;	
+						  else	
+							success_ration = '0';
+							
 						 if(key == 'coupon_id')
-							data += " <li class='item-content'><div class='item-media'><i class='icon icon-form-name'></i></div><div class='item-inner'><div class='item-title'><i class='fa fa-tags' style='font-size:20px;color:#007aff;'></i> <a href='"+ result[i].coupon_id+ "'>" + result[i].coupon_title +"</a></div></div></li>";
+							data 	+= 	'<div class="row main-store-content no-gutter">'
+									+		'<div class="inner-row-main-store-content">'
+									+				'<div class="col-80 stores-heading">'
+									+					'<h2 class="no-margin"><a href="coupon-detail.html?coupon_id=' + result[i].coupon_id +'">' + result[i].coupon_title +'</a></h2>'
+									+				'</div>'
+									+				'<div class="col-20 save">'
+									+					'<h2 class="no-margin">SAVE</h2>'
+									+						'<i class="fa fa-star" id="star-icon"></i>'
+									+				'</div>'
+									+		'</div>'
+									+		'<!--end inner-row-main-store-content -->'
+									+		'<div class="col-100 store-content no-margin">'
+									+			'<h3>' + result[i].store_name +'</h3>'
+									+			'<p>' + result[i].coupon_description +'</p>'
+									+		'</div>'
+									+		'<div class="col-30 success no-margin">'
+									+			'<h5>' + success_ration +'% Success</h5>'
+									+		'</div>'
+									+		'<div class="col-60 Expiry-date no-margin">'
+									+			'<h5>' + end_date +'</h5>'
+									+		'</div>'
+									+	'</div>'
+									+	'<!--end main-store-content no-gutter -->';
+									
+							store_heading = result[i].store_name;
+							store_heading_main = result[i].store_name + ' Coupons & Offers';
+							store_logo_main = '<img src="'+ result[i].store_logo +'" width="100%" height="100%"/>';		
 					   });
 					}
+					//alert(data);
 					
-					$('#category_coupons_list').html(data);
+					$('#store_coupons_list1').html(data);
+					$('#store_name_heading').html(store_heading);
+					$('#store_heading_main').html(store_heading_main);
+					$('#store_logo_div').html(store_logo_main);
 				},
 			error: function (request, error) {
 					alert('Error ' + error);
