3267b675c263169ac6547dfbf63ebf8dd6081626
kashifsultani84 <kashifsultani84@gmail.com>
2017-05-23 09:44:43 +0500

    may23

--- js/my-app.js
+++ js/my-app.js
@@ -81,8 +81,8 @@ $$(document).on('pageInit', function (e) {
 	if (page.name === 'index') {
 		
 
-		var str="something you want to copy to clipboard";
-		var result = community.clipboard.setText(str);
+		//var str="something you want to copy to clipboard";
+		//var result = community.clipboard.setText(str);
 		//result shows the bytes copied into clipboard, if result=="-1",then something is wrong. 
 		 
 		//var str = community.clipboard.getText;
